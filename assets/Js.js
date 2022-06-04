bf9c932fb4f3b4c2d0671ff9b80b1e83c90e1794
longdev09 <longbachnguyen09dev@gmail.com>
2022-06-04 22:34:40 +0700

    update

--- assets/Js.js
+++ assets/Js.js
@@ -16,4 +16,39 @@ const headercontacts =  document.querySelector('.header-contacts');
             headercontacts.classList.remove('close-heade-contacts');
         }
     })
-    
+
+    // Tăng 
+    var click = 0;
+    function clickIncrease() {
+        click+= 1;
+        document.getElementById("result").value = click;
+        
+    }
+    //Giảm
+    function clickDecrease() {
+        if(click == 0)
+            document.getElementById("result").value = 0;
+        else
+        {
+            click-= 1;
+            document.getElementById("result").value = click;
+        }    
+    }
+
+// Dong mo dang nhap
+
+var headerAccDropdown =  document.querySelector('.header-acc-dropdown');
+var iconHeaderList = document.querySelector('.icon-header-list');
+iconHeaderList.onclick = function OpenHeaderAccDropdown()
+{   var isOpen = headerAccDropdown.clientHeight === 0;
+    if( isOpen)
+    {
+        headerAccDropdown.style.display='block'
+    }
+    else
+    {
+        headerAccDropdown.style.display='none'
+
+    }
+}
+
